32f5412bdcce0e3594dbb8c60167cc44551e07bb
Naved <naved1234@gmail.com>
2018-07-18 17:30:51 +0530

    added some more stl ds

--- stl.cpp
+++ stl.cpp
@@ -28,4 +28,53 @@ int main() {
     // } else {
     //     cout << s1 << " is not equal to " << s2 << endl;
     // }
+
+    //Pair
+    // pair <int, char> p;
+    // p = make_pair(2, 'b');
+    // pair <int, char> p1(1, 'a');
+
+    // cout << p1.first << " " << p1.second << endl;
+    // cout << p.first << " " << p.second << endl;
+
+    //Set
+    // set <int> s;
+    // int a[] = {1, 2, 3, 4, 5, 6, 5};
+    // for ( int i = 0; i < 7; i++ ) {
+    //     s.insert(a[i]);
+    // }
+
+    // set <int> :: iterator it;
+
+    // for ( it = s.begin(); it != s.end(); it++) {
+    //     cout << *it << " ";
+    // }
+
+    // if (s.find(6) == s.end()) {
+    //     cout << "Not found" << endl;
+    // } else {
+    //     cout << "Found" << endl;
+    // }
+
+    //Map
+    // int a[] = {1, 2, 3, 4, 5, 6, 5};
+    // map <int, int> m;
+
+    // for( int i = 0; i < 7; i++) {
+    //     m[a[i]]++;
+    // }
+
+    // map <int, int> :: iterator it;
+
+    // for ( it = m.begin(); it != m.end(); it++) {
+    //     cout << it -> first << " " << it -> second << endl;
+    // }
+    // cout << endl;
+    // m.erase(1);
+    // for ( it = m.begin(); it != m.end(); it++) {
+    //     cout << it -> first << " " << it -> second << endl;
+    // }
+
+    
+
 }
